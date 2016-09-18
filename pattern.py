82b4c16f3718a1a0eb57805532ba13f087e85a92
asaf <smartush78@gmail.com>
2016-09-18 15:09:06 +0300

    with feature detection with homegraphy

--- pattern.py
+++ pattern.py
@@ -1,33 +1,70 @@
 import numpy as np
 import cv2
 
+MIN_MATCH_COUNT = 10
+
+
 from matplotlib import pyplot as plt
 
-img1 = cv2.imread('just1(2).jpg',0)          # queryImage
+# FLANN_INDEX_KDTREE = 0
+# index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+# search_params = dict(checks = 50)
+
+img1 = cv2.imread('Lotus-logo.jpg',0)          # queryImage
 #img2 = cv2.imread('lambiCollection.png',0) # trainImage
 #img2 = cv2.imread('coinsCollection.jpg',0) # trainImage
-img2 = cv2.imread('real.png',0) # trainImage
+img2 = cv2.imread('real.jpg',0) # trainImage
 
 
 cv2.ocl.setUseOpenCL(False)
 # Initiate SIFT detector
-sift = cv2.xfeatures2d.SIFT_create()
+orb = cv2.ORB_create()
 
 # find the keypoints and descriptors with SIFT
-kp1, des1 = sift.detectAndCompute(img1,None)
-kp2, des2 = sift.detectAndCompute(img2,None)
+kp1, des1 = orb.detectAndCompute(img1,None)
+kp2, des2 = orb.detectAndCompute(img2,None)
+print 'Des1',des2
 
-# BFMatcher with default params
-bf = cv2.BFMatcher()
-matches = bf.knnMatch(des1,des2, k=2)
 
-# Apply ratio test
+# flann = cv2.FlannBasedMatcher(index_params,{ })
+
+# matches = flann.knnMatch(des1,des2,k=2)
+bfmatcher = cv2.BFMatcher()
+matches = bfmatcher.knnMatch(des1,des2,k=2)
+
+# store all the good matches as per Lowe's ratio test.
 good = []
 for m,n in matches:
-    if m.distance < 0.75*n.distance:
-        good.append([m])
+    if m.distance < 0.7*n.distance:
+        good.append(m)
+
+if len(good)>MIN_MATCH_COUNT:
+    src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+
+    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+    matchesMask = mask.ravel().tolist()
+
+    h,w = img1.shape
+    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+    dst = cv2.perspectiveTransform(pts,M)
+
+    img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+
+else:
+    print "Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT)
+    matchesMask = None
+
+draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+                   singlePointColor = None,
+                   matchesMask = matchesMask, # draw only inliers
+                   flags = 2)
+
+img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
+
+plt.imshow(img3, 'gray'),plt.show()
 
 # cv2.drawMatchesKnn expects list of lists as matches.
-img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None)
+#img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None)
 
-plt.imshow(img3),plt.show()
+#plt.imshow(img3),plt.show()
