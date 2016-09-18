1be30dc3324cbf76a20379cba745e821483ef521
asaf <smartush78@gmail.com>
2016-09-18 18:16:17 +0300

    item counter iterator

--- pattern.py
+++ pattern.py
@@ -9,62 +9,67 @@ from matplotlib import pyplot as plt
 # FLANN_INDEX_KDTREE = 0
 # index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
 # search_params = dict(checks = 50)
-
+lambiCounter = 0
+#img1 = cv2.imread('just1.jpg',0) # trainImage
+#img2 = cv2.imread('real.png',0) # trainImage
 img1 = cv2.imread('lambi-logo-2.png',0)          # queryImage
-#img2 = cv2.imread('lambiCollection.png',0) # trainImage
-#img2 = cv2.imread('coinsCollection.jpg',0) # trainImage
 img2 = cv2.imread('lambiCollection.png',0) # trainImage
+x = 1
+while x == 1:
+
+    cv2.ocl.setUseOpenCL(False)
+    # Initiate SIFT detector
+    surf = cv2.xfeatures2d.SURF_create()
+
+    # find the keypoints and descriptors with SIFT
+    kp1, des1 = surf.detectAndCompute(img1,None)
+    kp2, des2 = surf.detectAndCompute(img2,None)
+    #print 'Des1',des2
 
 
-cv2.ocl.setUseOpenCL(False)
-# Initiate SIFT detector
-orb = cv2.ORB_create()
+    # flann = cv2.FlannBasedMatcher(index_params,{ })
 
-# find the keypoints and descriptors with SIFT
-kp1, des1 = orb.detectAndCompute(img1,None)
-kp2, des2 = orb.detectAndCompute(img2,None)
-print 'Des1',des2
+    # matches = flann.knnMatch(des1,des2,k=2)
+    bfmatcher = cv2.BFMatcher()
+    matches = bfmatcher.knnMatch(des1,des2,k=2)
+
+    # store all the good matches as per Lowe's ratio test.
+    good = []
+    for m,n in matches:
+        if m.distance < 0.7*n.distance:
+            good.append(m)
+
+    if len(good)>MIN_MATCH_COUNT:
+        lambiCounter = lambiCounter + 1
+        src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+        dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+
+        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+        matchesMask = mask.ravel().tolist()
+
+        h,w = img1.shape
+        pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+        dst = cv2.perspectiveTransform(pts,M)
+
+        #img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+        img2 = cv2.fillPoly(img2,[np.int32(dst)],255,8, 0)
+
+    else:
+        print "Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT)
+        print 'number of lambi packs are ' , lambiCounter
+        matchesMask = None
+        x = 0
+
+    draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+                       singlePointColor = None,
+                       matchesMask = matchesMask, # draw only inliers
+                       flags = 2)
 
 
-# flann = cv2.FlannBasedMatcher(index_params,{ })
+    #print cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+    img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
 
-# matches = flann.knnMatch(des1,des2,k=2)
-bfmatcher = cv2.BFMatcher()
-matches = bfmatcher.knnMatch(des1,des2,k=2)
+    #crop_img = img3[92:224, 267:570]
 
-# store all the good matches as per Lowe's ratio test.
-good = []
-for m,n in matches:
-    if m.distance < 0.7*n.distance:
-        good.append(m)
-
-if len(good)>MIN_MATCH_COUNT:
-    src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
-    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
-
-    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
-    matchesMask = mask.ravel().tolist()
-
-    h,w = img1.shape
-    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-    dst = cv2.perspectiveTransform(pts,M)
-
-    img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
-
-else:
-    print "Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT)
-    matchesMask = None
-
-draw_params = dict(matchColor = (0,255,0), # draw matches in green color
-                   singlePointColor = None,
-                   matchesMask = matchesMask, # draw only inliers
-                   flags = 2)
-
-img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
-
-plt.imshow(img3, 'gray'),plt.show()
-
-# cv2.drawMatchesKnn expects list of lists as matches.
-#img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None)
-
-#plt.imshow(img3),plt.show()
+    #plt.imshow(img3, 'gray'),plt.show()
+    plt.imshow(img2, 'gray'),plt.show()
