089b075a864eec8b2ffe81e31f7840b73c90ca27
asaf <smartush78@gmail.com>
2016-09-22 12:53:58 +0300

    pattern2 video stream

--- pattern2.py
+++ pattern2.py
@@ -1,38 +1,39 @@
 import numpy as np
 import cv2
 
-MIN_MATCH_COUNT = 20
-
-
 from matplotlib import pyplot as plt
 
-
+MIN_MATCH_COUNT = 100
 lambiCounter = 0
-#img1 = cv2.imread('jus1.png',0) # trainImage
-#img2 = cv2.imread('real.png',0) # trainImage
-img1 = cv2.imread('lambi-logo-2.png',0)          # queryImage
-img2 = cv2.imread('lambiCollection.png',0) # trainImage
-x = 1
-while x == 1:
 
-    cv2.ocl.setUseOpenCL(False)
-    # Initiate SIFT detector
-    surf = cv2.xfeatures2d.SURF_create()
+img1 = cv2.imread('book.png',0)
 
-    # find the keypoints and descriptors with SIFT
-    kp1, des1 = surf.detectAndCompute(img1,None)
-    kp2, des2 = surf.detectAndCompute(img2,None)
-    #print 'Des1',des2
-    # flann = cv2.FlannBasedMatcher(index_params,{ })
+cap = cv2.VideoCapture(0)
+
+cv2.ocl.setUseOpenCL(False)
+# Initiate SIFT detector
+surf = cv2.xfeatures2d.SURF_create()
+kp1, des1 = surf.detectAndCompute(img1,None)
+
+
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    kp2, des2 = surf.detectAndCompute(gray,None)
 
     bfmatcher = cv2.BFMatcher()
     matches = bfmatcher.knnMatch(des1,des2,k=2)
 
-    # store all the good matches as per Lowe's ratio test.
     good = []
     for m,n in matches:
         if m.distance < 0.7*n.distance:
             good.append(m)
+
+    #print "GOOD --> ", good
 
     if len(good)>MIN_MATCH_COUNT:
         lambiCounter = lambiCounter + 1
@@ -47,7 +48,7 @@ while x == 1:
         dst = cv2.perspectiveTransform(pts,M)
 
         #img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
-        img2 = cv2.fillPoly(img2,[np.int32(dst)],255,8, 0)
+        #img2 = cv2.fillPoly(img2,[np.int32(dst)],255,8, 0)
         print 'number of lambi packs are ' , lambiCounter
 
     else:
@@ -62,10 +63,19 @@ while x == 1:
                        flags = 2)
 
 
-    #print cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
-    img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
+    #cv2.polylines(frame,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+    frame = cv2.drawMatches(img1,kp1,gray,kp2,good,None,**draw_params)
 
     #crop_img = img3[92:224, 267:570]
 
-    plt.imshow(img3, 'gray'),plt.show()
-    plt.imshow(img2, 'gray'),plt.show()
+    ##plt.imshow(frame, 'gray'),plt.show()
+
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+
+cv2.destroyAllWindows()
